32e8715da34b124a5df8178b54c23b92cb451576
ssedefkadifeci <sedef.kadifeci@std.hku.edu.tr>
2022-06-28 12:14:59 +0300

    Date Time Calendar
    Calendar shows the dates and times with the buttons

---
+++ DateTimeCalendar/DateFunct/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DateFunct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(16, 15);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(148, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 64);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CompareTo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(187, 15);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 64);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DaysInMonth";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(232, 64);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "TimeSpan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(340, 64);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "AddDays";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 116);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 28);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Example";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(124, 116);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 28);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "DayOfYear";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(364, 116);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(244, 116);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 28);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Currency";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(16, 165);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Compare";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 399);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ DateTimeCalendar/DateFunct/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateFunct
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime d1 = dateTimePicker1.Value;
+            string s1 = d1.ToLongDateString();
+            MessageBox.Show(s1);
+
+            DateTime d2 = DateTime.Parse(s1);
+            //dateTimePicker2.Value = d2;
+            //DateTime d3 = DateTime.Parse("12.12.2019");
+            //DateTime d4 = DateTime.Parse("12.12.2019");
+            //int sonuc = d3.CompareTo(d4);
+            //MessageBox.Show(sonuc.ToString());
+
+            //DateTime d5 = dateTimePicker1.Value;
+            //DateTime d6 = dateTimePicker2.Value;
+            //int result = d5.CompareTo(d6);
+            //MessageBox.Show(result.ToString());
+
+            //DateTime d7 = dateTimePicker1.Value;
+            //int result2 = d5.CompareTo(DateTime.Now);
+            //MessageBox.Show(result2.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //DateTime da = dateTimePicker1.Value;
+            //int sonuc = DateTime.DaysInMonth(da.Year, da.Month);
+            //MessageBox.Show(sonuc.ToString());
+
+            int sonuc = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+            MessageBox.Show(sonuc.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DateTime dateBirth = DateTime.Parse("09.05.1997 23:00:00");
+            DateTime dateToday = DateTime.Now;
+            TimeSpan ts = dateToday.Subtract(dateBirth);
+            MessageBox.Show(ts.Days.ToString() + " day " + ts.Hours.ToString() + " Hour " +
+                ts.Minutes.ToString() + " Minutes " + ts.Seconds.ToString() + " Seconds ");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //    DateTime newDate = DateTime.Now.AddDays(45);
+            //    MessageBox.Show(newDate.ToString("dd.MM.yyyy hh:mm:ss tt"));
+
+            DateTime d = DateTime.Parse("20.12.2019");
+            DateTime newd = d.AddDays(3);
+            MessageBox.Show(newd.ToString("dd.MM.yyyy"));
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DateTime newDate = dateTimePicker1.Value.AddDays(30);
+            //MessageBox.Show(newDate.DayOfWeek.ToString());
+            if(newDate.DayOfWeek.ToString()=="Saturday")
+            {
+                newDate= newDate.AddDays(2);
+            }
+            else if (newDate.DayOfWeek.ToString() == "Sunday")
+            {
+                newDate = newDate.AddDays(1);
+            }
+            MessageBox.Show(newDate.DayOfWeek.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DateTime d1 = DateTime.Now;
+            MessageBox.Show(d1.DayOfYear.ToString());
+            MessageBox.Show(d1.ToString("dd-MM-yyyy"));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double Value = Convert.ToDouble(textBox1.Text);
+            string MAAS = String.Format("{0:c}", Value);
+            MessageBox.Show(MAAS);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            DateTime dtime1 = DateTime.Parse("14.07.2011");
+            DateTime dtime2 = DateTime.Parse("14.08.2011");
+            int result = DateTime.Compare(dtime1, dtime2);
+            MessageBox.Show(result.ToString());
+        }
+    }
+}
